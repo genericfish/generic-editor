7904bd57534dc539426b60402fa27d9f2e1ede3d
WKent01 <kentwoolner@gmail.com>
2020-11-13 22:58:20 -0600

    add server

--- image-server/server.py
+++ image-server/server.py
@@ -1,1 +1,24 @@
-data here
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import socketserver
+
+PORT = 8000
+
+class Handler(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_POST(self):
+        self._set_headers()
+        
+
+
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
+
